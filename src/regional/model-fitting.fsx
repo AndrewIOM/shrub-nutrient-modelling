3e688e555aeebd033edc0bb628fc4c564af15d82
AndrewIOM <AndrewIOM@users.noreply.github.com>
2024-07-30 12:11:42 +0100

    [WIP] read isotope data

--- src/regional/model-fitting.fsx
+++ src/regional/model-fitting.fsx
@@ -34,7 +34,6 @@ model system within.
 
 #load "model/model.fsx"
 
-
 (**
 ### Setting up a *Bristlecone engine*
 
@@ -52,11 +51,8 @@ let engine =
     |> Bristlecone.withContinuousTime Integration.MathNet.integrate
     |> Bristlecone.withOutput output
     |> Bristlecone.withConditioning Conditioning.RepeatFirstDataPoint
-    |> Bristlecone.withCustomOptimisation (
-        Optimisation.MonteCarlo.SimulatedAnnealing.fastSimulatedAnnealing
-            0.01
-            true
-            { Optimisation.MonteCarlo.SimulatedAnnealing.AnnealSettings<float>.Default with
+        |> Bristlecone.withCustomOptimisation (Optimisation.MonteCarlo.SimulatedAnnealing.fastSimulatedAnnealing 0.01 true
+            { Optimisation.MonteCarlo.SimulatedAnnealing.AnnealSettings<float>.Default with 
                 InitialTemperature = 100.
                 TemperatureCeiling = Some 100.
                 HeatRamp = (fun t -> t + 5.00)
@@ -65,31 +61,126 @@ let engine =
                 TuneLength = 1000
                 AnnealStepLength =
                     (fun x n ->
-                        Optimisation.MonteCarlo.SimulatedAnnealing.EndConditions.improvementCount 5000 250 x n
-                        || Optimisation.EndConditions.afterIteration 10000 x n) }
-    )
+                        (* Optimisation.MonteCarlo.SimulatedAnnealing.EndConditions.improvementCount 5000 250 x n
+                        || *) Optimisation.EndConditions.afterIteration 10000 x n) }
+        )
 
 (**
-### Load in the real dataset
+### Read in ring width, isotope, and weather station data
 
 Here, we are using the Bristlecone.Dendro package to 
 read in dendroecological data. For other problems, any
 method to wrangle the data into a `TimeSeries` is acceptable.
-
 *)
 
 open Bristlecone.Dendro
 
-let dataset =
-    let plants =
-        Data.PlantIndividual.loadRingWidths (__SOURCE_DIRECTORY__ + "/../data/yamal-rw.csv")
-
-    let isotopeData =
-        Data.PlantIndividual.loadLocalEnvironmentVariable (__SOURCE_DIRECTORY__ + "/../data/yuribei-d15N-imputed.csv")
-
-    plants |> PlantIndividual.zipEnvMany "N" isotopeData
+let ringWitdhDataset =
+    Data.PlantIndividual.loadRingWidths (__SOURCE_DIRECTORY__ + "/../data/yamal-rw.csv")
 
 (**
+#### Read in air temperature data
+*)
+
+open FSharp.Data
+
+type TemperatureData = CsvProvider<"../../data/yamal-mean-temperatures.csv">
+
+let jjaTemperaturesByStation =
+    let maxTemperatures = TemperatureData.Load "../../data/yamal-mean-temperatures.csv"
+    maxTemperatures.Rows
+    |> Seq.groupBy(fun r -> r.Station)
+    |> Seq.map(fun (station,r) -> station, r |> Seq.map(fun r -> ( float r.JJA + 273.15, System.DateTime.Create 31 12 r.Year)) |> TimeSeries.fromObservations)
+    |> Seq.toList
+
+(**
+#### Read in two isotope datasets at three-yearly resolution
+
+For this analysis, we conduct model-fitting on a common three-year timeline
+across all shrubs ending at [2009 - 2012]. The below code ensures that data
+resolution is lowered when it is higher than the three-year bins. For example,
+some shrubs have higher resolution (annual) d15N data from 1980.
+*)
+
+type Regional15N = CsvProvider<"../../data/yamal-d15N-lowres.csv">
+type YuribeiD15N = CsvProvider<"../../data/yuribei-d15N-imputed.csv">
+
+let bins = seq { 1900 .. 3 .. 2018 }
+let regionalD15N = Regional15N.Load (__SOURCE_DIRECTORY__ + "/../data/yamal-d15N-lowres.csv")
+let yuribeiD15N = YuribeiD15N.Load (__SOURCE_DIRECTORY__ + "/../data/yuribei-d15N-imputed.csv")
+
+/// Converts input data into 3-year binned d15N and increment data
+let threeYearToAnnual (plant:PlantIndividual.PlantIndividual) =
+
+    let growth =
+        match plant.Growth with
+        | PlantIndividual.PlantGrowth.RingWidth x ->
+            match x with
+            | GrowthSeries.Absolute rw -> rw
+            | GrowthSeries.Cumulative(_) -> failwith "Not Implemented"
+            | GrowthSeries.Relative(_) -> failwith "Not Implemented"
+        | _ -> failwith "not implemented"
+
+    let (isotopeByBin:(int*float) list list) = 
+        bins
+        |> Seq.choose(fun binStart ->
+            let lowResBins =
+                regionalD15N.Rows
+                |> Seq.where(fun n -> n.Shrub = plant.Identifier.Value)
+                |> Seq.where(fun n -> n.BinLatest <= (binStart + 2) && n.BinOldest >= binStart)
+                |> Seq.map(fun r -> (r.BinOldest, r.BinLatest, r.D15N))
+
+            let highResBins =
+                yuribeiD15N.Rows
+                |> Seq.where(fun n -> n.``Plant Code`` = plant.Identifier.Value)
+                |> Seq.where(fun n -> n.Date.Year <= (binStart + 2) && n.Date.Year >= binStart)
+                |> Seq.map(fun r -> (r.Date.Year, r.Date.Year, (float r.``Predictor 2``) ))
+
+            let allData = 
+                highResBins 
+                |> Seq.append lowResBins 
+                |> Seq.sortBy (fun (s,_,_) -> s)
+                |> Seq.filter(fun (_,_,x) -> not <| System.Double.IsNaN x)
+
+            if allData |> Seq.isEmpty then None
+            else
+                match highResBins |> Seq.length with
+                | i when i >= 0 && i < 3 ->
+                    try
+                        let weightedAverage =
+                            let weights = 
+                                let increments = allData |> Seq.map (fun (start,en,_) -> [ start .. en ] |> List.sumBy(fun y -> growth |> TimeSeries.findExact (System.DateTime(y, 12, 31)) |> fst |> removeUnit ))
+                                let totalIncrement = increments |> Seq.sum
+                                increments |> Seq.map(fun i -> i / totalIncrement)
+                            printfn "Weights are %A" weights
+                            allData
+                            |> Seq.zip weights
+                            |> Seq.averageBy(fun (weight,(s,e,d15n)) -> d15n * weight )
+                        Some <| ([ binStart .. binStart + 2 ] |> List.map (fun y -> y, weightedAverage))
+                    with | _ -> None
+                | _ ->
+                    highResBins 
+                    |> Seq.map(fun (x,y,z) -> (x, z) ) 
+                    |> Seq.toList
+                    |> Some )
+        |> Seq.toList
+    List.concat isotopeByBin
+
+let dataset =
+    ringWitdhDataset
+    |> Seq.map(fun plant ->
+        let isotope3Year = 
+            threeYearToAnnual plant
+            //lowerResolution plant
+            |> List.map (fun (x,y) -> (y, System.DateTime(x, 12, 31)))
+            |> TimeSeries.fromObservations
+        plant |> PlantIndividual.zipEnv "N" isotope3Year )
+
+
+(**
+The final dataset in `dataset` includes each plant individual with
+its associated isotope data ('N'), both at three-yearly resoluion.
+
 ### Model-fitting to real data
 
 In this step, we will apply our `EstimationEngine` to the real
@@ -144,7 +235,7 @@ let startValues (startDate: System.DateTime) (plant: PlantIndividual.PlantIndivi
             | _ -> invalidOp "Not applicable"
         | _ -> invalidOp "Not applicable"
 
-    let initialMass = initialRadius |> Allometric.Proxies.toBiomassMM
+    let initialMass = initialRadius |> ModelComponents.Proxies.toBiomassMM
     let initialNitrogen = plant.Environment.[(code "N").Value].Head |> fst
 
     [ ((code "x").Value, initialRadius |> removeUnit)
@@ -168,14 +259,58 @@ module Config =
     let endWhen = Optimisation.EndConditions.afterIteration 100000
 
 
+let enforceMinimumRadius minSize (plant:PlantIndividual.PlantIndividual) =
+    let bounded = 
+        match plant.Growth with
+        | RingWidth rw ->
+            match rw with
+            | Cumulative g -> 
+                g 
+                |> TimeSeries.toObservations 
+                |> Seq.skipWhile (fun (x,_) -> x < minSize) 
+                |> TimeSeries.fromObservations 
+                |> Cumulative
+            | _ -> invalidOp "Not implemented"
+        | _ -> invalidOp "Not implemented"
+    { plant with Growth = bounded |> RingWidth }
+
+let tailGrowth (plant:Plant.PlantIndividual) =
+    let bounded = 
+        match plant.Growth with
+        | RingWidth rw ->
+            match rw with
+            | Cumulative g -> 
+                g 
+                |> TimeSeries.toObservations 
+                |> Seq.tail
+                |> TimeSeries.fromObservations 
+                |> Cumulative
+            | _ -> invalidOp "Not implemented"
+        | _ -> invalidOp "Not implemented"
+    { plant with Growth = bounded |> RingWidth }
+
+
 // Function to scaffold work packages
-let workPackages shrubs (hypotheses: Hypotheses.Hypothesis list) engine saveDirectory =
+let workPackages (shrubs: PlantIndividual.PlantIndividual seq) (hypotheses: Hypotheses.Hypothesis list) engine saveDirectory =
     seq {
         for s in shrubs do
 
+            let tMax = 
+                if s.Identifier.Value.Contains "S8"
+                then jjaTemperaturesByStation |> Seq.find(fun (s,_) -> s = "Marre Sale") // A Varandei shrub
+                else jjaTemperaturesByStation |> Seq.find(fun (s,_) -> s = "Hoseda Hard") // A Yamal shrub
+
             // 1. Arrange the subject and settings
-            let shrub = s |> PlantIndividual.toCumulativeGrowth
-            let common = shrub |> PlantIndividual.keepCommonYears
+            let shrub = 
+                s
+                |> PlantIndividual.toCumulativeGrowth
+                |> enforceMinimumRadius 1.53<mm>
+                |> PlantIndividual.zipEnv "T[max]" (snd tMax)
+            let common =
+                shrub 
+                |> tailGrowth
+                |> PlantIndividual.keepCommonYears
+                |> PlantIndividual.zipEnv "T[max]" (snd tMax)
             let startDate = (common.Environment.[(code "N").Value]).StartDate |> snd
             let startConditions = startValues startDate shrub
             let e = engine |> Bristlecone.withConditioning (Conditioning.Custom startConditions)
@@ -203,10 +338,7 @@ let workPackages shrubs (hypotheses: Hypotheses.Hypothesis list) engine saveDire
                         }
     }
 
-let work = workPackages dataset hypotheses engine Config.resultsDirectory
+let work = workPackages dataset Model.hypotheses engine Config.resultsDirectory
 
-// Calling this function will run all of the analyses, but for this
-// example script we won't run them here.
-let run () =
-    work
-    |> Seq.iter (Orchestration.OrchestrationMessage.StartWorkPackage >> orchestrator.Post)
+work
+|> Seq.iter (Orchestration.OrchestrationMessage.StartWorkPackage >> orchestrator.Post)
